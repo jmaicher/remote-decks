1344b233194c7925344fe3cc724e9457a604d0db
Julian Maicher <mail@jmaicher.de>
2012-11-03 13:05:06 +0100

    Add views (jade), stylesheets (stylus) and jquery

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
 var express = require('express'),
+    stylus = require('stylus'),
     http = require('http');
+
 
 // ###########################
 // ## Setup & Configuration ##
@@ -7,20 +9,40 @@ var express = require('express'),
 
 var app = express();
 
+// serve static assets
+var assets_path = __dirname + '/public';
+app.use(express.static(assets_path));
+// use stylus for stylesheets
+app.use(stylus.middleware({
+  debug: true,
+  src: assets_path,
+  compile: function(str, path) {
+    return stylus(str)
+      .set('filename', path)
+  }
+}));
+
+// rendering
+var view_path = __dirname + '/views';
+app.set('views', view_path);
+app.set('view engine', 'jade');
+
+// logging
+app.use(express.logger());
+
 
 // ####################
 // ## Routes ##########
 // ####################
 
 app.get('/', function(req, res) {
-  res.send('Hello World');
+  res.render('index');
 });
 
 
 // ######################
 // ## Run kitten, run ###
 // ######################
-
 
 var port = process.env.PORT || 3000;
 http.createServer(app).listen(port, function() {
