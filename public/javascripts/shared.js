56b8f81a96b7239276f186b796c04236db115acc
Julian Maicher <mail@jmaicher.de>
2012-11-03 19:32:02 +0100

    Add deck.js and partial containing slides

--- public/javascripts/shared.js
+++ public/javascripts/shared.js
@@ -1,12 +1,12 @@
 $(function() {
 
-  console.log(window.test.hello_world());
-
   var socket = io.connect('/global');
 
   socket.on('server-news', function(data) {
     console.log(data.i);
     socket.emit('client-news', { i: data.i + 1 });
   });
+
+  $.deck('.slide');
 
 });
