ce72c63ba568dca1620e3d24a3b4b5818a684148
Julian Maicher <mail@jmaicher.de>
2012-11-04 15:28:13 +0100

    Disallow deck control for spectators

--- public/javascripts/spectator.js
+++ public/javascripts/spectator.js
@@ -3,8 +3,14 @@ $(function() {
   $.deck('.slide');
   $.deck('go', session.get('slide'));
 
-  var socket = io.connect('/spectator');
+  // disallow deck controls for spectators
+  $d = $(document);
+  $d.unbind('keydown.deck');
+  $d.unbind('touchstart.deck');
+  $d.unbind('touchmove.deck');
+  $d.unbind('touchend.deck');
 
+  var socket = io.connect('/spectator');
   socket.on('slide.change', function(data) {
     $.deck('go', data.to);
   });
